cee809b209b97cc8a0d1d2c5ccee220aeca0eadb
Fenil <fabperry04@gmail.com>
2024-02-01 23:01:45 +0530

    4 commit

---
+++ OneDrive/Desktop/git practice/fourth.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main
+{
+    int a , b;
+    printf(" A is 5");
+    printf(" b is 50");
+    printf(" A is 5");
+    printf(" b is 50");    
+    printf(" hello world");
+    printf(" hello world");
+    printf(" hello world");
+    return 0;
+}
